255e4562207f5d32ea987b474f9efecaf176d7c4
CarlosDanielBenitez <cdanibenz@gmail.com>
2023-10-12 16:38:41 -0300

    Features: added slider component and implement home

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Header from './components/header'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+import ProductDetail from './pages/product-detail'
+function App() {
+  return (
+    <div>
+      <Header logo="Ds"/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products/:productId' element={<ProductDetail />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
